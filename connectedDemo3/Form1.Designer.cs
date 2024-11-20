14833e701b65c2422952010aec2543889ba38329
Lalitpach <lalitpachpande111@gmail.com>
2024-11-20 10:27:48 +0800

    Add project files.

---
+++ connectedDemo3/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace connectedDemo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnshowall = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 142);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 227);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 314);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "salary";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(200, 44);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(270, 38);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(200, 142);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(270, 38);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(200, 227);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(270, 38);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(200, 311);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(270, 38);
+            this.txtSalary.TabIndex = 7;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(45, 383);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(119, 38);
+            this.btnsave.TabIndex = 8;
+            this.btnsave.Text = "save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(200, 383);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(119, 38);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Location = new System.Drawing.Point(340, 383);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(119, 38);
+            this.btnsearch.TabIndex = 10;
+            this.btnsearch.Text = "search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(485, 383);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(119, 38);
+            this.btndelete.TabIndex = 11;
+            this.btndelete.Text = "delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnshowall
+            // 
+            this.btnshowall.Location = new System.Drawing.Point(627, 383);
+            this.btnshowall.Name = "btnshowall";
+            this.btnshowall.Size = new System.Drawing.Size(221, 38);
+            this.btnshowall.TabIndex = 12;
+            this.btnshowall.Text = "show all emp";
+            this.btnshowall.UseVisualStyleBackColor = true;
+            this.btnshowall.Click += new System.EventHandler(this.btnshowall_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(627, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(307, 305);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1700, 872);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnshowall);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnshowall;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
